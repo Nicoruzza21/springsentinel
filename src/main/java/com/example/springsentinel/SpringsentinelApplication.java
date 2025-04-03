6240699c6bfe4ae7a18d9eb3b3ccd3be3108d4dc
Nicholas Ruzza Ruiz <nruzza@stu.edu>
2025-04-03 06:27:00 +0200

    initial commit

---
+++ src/main/java/com/example/springsentinel/SpringsentinelApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsentinelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsentinel/SpringsentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsentinelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
